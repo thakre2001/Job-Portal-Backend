99a94b799f70b074d5b71383d420217351dec4a0
Shubham Thakre <shubhamthakre43@gmail.com>
2025-08-04 13:41:12 +0530

    Added netlify frontend link

--- src/main/java/com/myproject/jobportal/config/SecurityConfig.java
+++ src/main/java/com/myproject/jobportal/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://jobmatchweb.netlify.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
